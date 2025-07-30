433b282d7fb901b86a9f2d0efd846b9b34ab8cbc
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2025-07-31 04:07:53 +0545

    AdminDashBoardSidebar

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -23,15 +23,20 @@ import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
 import ButtonLoading from '@/components/Application/ButtonLoading'
 import Link from 'next/link'
-import { WEBSITE_REGISTER, WEBSITE_RESETPASSWORD } from '@/routes/WebsiteRoute'
+import { USER_DASHBOARD, WEBSITE_REGISTER, WEBSITE_RESETPASSWORD } from '@/routes/WebsiteRoute'
 import axios from 'axios'
 import { showToast } from '@/lib/showToast'
 import OTPVerification from '@/components/Application/OTPVerification'
 import { useDispatch } from 'react-redux'
 import { login } from '@/store/reducer/authReducer'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { ADMIN_DASHBOARD } from '@/routes/AdminPanelRoute'
 
 const LoginPage = () => {
   const dispatch = useDispatch()
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
   const [loading, setLoading] = useState(false);
   const [otpVerificationLoading, setOtpVerificationLoading] = useState(false);
   const [isTypePassword, setIsTypePassword] = useState(true);
@@ -84,6 +89,12 @@ const LoginPage = () => {
       showToast('success', otpResponse.message)
 
       dispatch(login(otpResponse.data))
+
+      if (searchParams.has('callback')) {
+        router.push(searchParams.get('callback'))
+      } else {
+        otpResponse.data.role === 'admin' ? router.push(ADMIN_DASHBOARD) : router.push(USER_DASHBOARD)
+      }
 
     } catch (error) {
       showToast('error', error.message)

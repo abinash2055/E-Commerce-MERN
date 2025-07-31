708a78c8a3b3e043edce8e3770c3e83cb7febbe5
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2025-07-31 22:04:10 +0545

    Logout Activated Done

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -4,16 +4,17 @@ import { cookies } from "next/headers";
 
 export async function POST(request) {
     try {
-        await connectDB()
-        const cookieStore = await cookies()
+        await connectDB(); 
+        const cookieStore = await cookies(); 
         cookieStore.delete("access_token");
+
         return response(
             true,
             200,
             "Logout Successfully."
-        )
+        );
 
     } catch (error) {
-        catchError(error)
+        catchError(error);
     }
 }
--- components/Application/Admin/LogoutButton.jsx
+++ components/Application/Admin/LogoutButton.jsx
@@ -3,37 +3,41 @@ import { showToast } from '@/lib/showToast'
 import { WEBSITE_LOGIN } from '@/routes/WebsiteRoute'
 import { logout } from '@/store/reducer/authReducer'
 import axios from 'axios'
-import Error from 'next/error'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import { AiOutlineLogout } from 'react-icons/ai'
 import { useDispatch } from 'react-redux'
 
 const LogoutButton = () => {
-
   const dispatch = useDispatch()
   const router = useRouter()
 
   const handleLogout = async () => {
     try {
-      const { data: logoutResponse } = await axios.post('api/auth/logout')
+      // Added below 2 lines to resolve my error....
+      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || window.location.origin;
+      const apiUrl = `${baseUrl}/api/auth/logout`;
+
+      // "{app/auth/logout} iss changed to apiUrl" to resolve error....
+      const { data: logoutResponse } = await axios.post(apiUrl);
+
       if (!logoutResponse.success) {
-        throw new Error(logoutResponse.message)
+        throw new Error(logoutResponse.message);
       }
- 
-      dispatch(logout())
-      showToast("success", logoutResponse.message)
-      router.push(WEBSITE_LOGIN)
+
+      dispatch(logout());
+      showToast("success", logoutResponse.message);
+      router.push(WEBSITE_LOGIN);
 
     } catch (error) {
-      showToast('error', error.message)
+      showToast('error', error.message);
     }
   }
 
   return (
     <DropdownMenuItem onClick={handleLogout} className="cursor-pointer">
-      <AiOutlineLogout color="red"/>
-        Logout
+      <AiOutlineLogout color="red" />
+      Logout
     </DropdownMenuItem>
   )
 }

34caf67bbd99be8e4a83dcd67cfafcdee3daed4b
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2025-08-10 23:12:40 +0545

    Admin Dashboard Started

---
+++ app/(root)/(admin)/admin/dashboard/LatestOrder.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+const LatestOrder = () => {
+  return (
+      <Table>
+          <TableHeader>
+              <TableRow>
+                  <TableHead>Invoice</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Method</TableHead>
+              </TableRow>
+          </TableHeader>
+          <TableBody>
+              <TableRow>
+                  <TableCell className="font-medium">INV001</TableCell>
+                  <TableCell>Paid</TableCell>
+                  <TableCell>Credit Card</TableCell>
+                  <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+          </TableBody>
+      </Table>
+  )
+}
+
+export default LatestOrder
--- app/(root)/(admin)/admin/dashboard/page.jsx
+++ app/(root)/(admin)/admin/dashboard/page.jsx
@@ -1,8 +1,77 @@
 import React from 'react'
+import CountOverview from './CountOverview'
+import QuickAdd from './QuickAdd'
+import { Card, CardHeader, CardContent } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { OrderOverview } from './OrderOverview'
+import { OrderStatus } from './OrderStatus'
+import LatestOrder from './LatestOrder'
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <div className='pt-5'>
+      <CountOverview />
+      <QuickAdd />
+
+      <div className="mt-10 flex lg:flex-nowrap flex-wrap gap-10">
+        <Card className="rounded-lg lg:w-[70%] w-full p-0">
+          <CardHeader className="py-3 border [.border-b]:pb-3">
+            <div className="flex justify-between items-center">
+              <span className="font-semibold">Order Overview</span>
+              <Button type="button">
+                <Link href="">View All</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <OrderOverview />
+          </CardContent>
+        </Card>
+        <Card className="rounded-lg lg:w-[30%] w-full p-0">
+          <CardHeader className="py-3 border [.border-b]:pb-3">
+            <div className="flex justify-between items-center">
+              <span className="font-semibold">Orders Status</span>
+              <Button type="button">
+                <Link href="">View All</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <OrderStatus />
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="mt-10 flex lg:flex-nowrap flex-wrap gap-10">
+        <Card className="rounded-lg lg:w-[70%] w-full p-0 block">
+          <CardHeader className="py-3 border [.border-b]:pb-3">
+            <div className="flex justify-between items-center">
+              <span className="font-semibold">Latest Order</span>
+              <Button type="button">
+                <Link href="">View All</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent className="pt-3">
+            <LatestOrder />
+          </CardContent>
+        </Card>
+        <Card className="rounded-lg lg:w-[30%] w-full p-0">
+          <CardHeader className="py-3 border [.border-b]:pb-3">
+            <div className="flex justify-between items-center">
+              <span className="font-semibold">Latest Review</span>
+              <Button type="button">
+                <Link href="">View All</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent>
+            
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   )
 }
 

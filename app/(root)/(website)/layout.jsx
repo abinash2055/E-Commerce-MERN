27a199a03161f3e84c6b96c2da503e30d528265c
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2025-08-12 21:27:45 +0545

    Header & Footer

--- app/(root)/(website)/layout.jsx
+++ app/(root)/(website)/layout.jsx
@@ -1,8 +1,23 @@
+import Footer from '@/components/Application/Website/Footer'
+import Header from '@/components/Application/Website/Header'
 import React from 'react'
+import { Kumbh_Sans } from 'next/font/google'
+
+const kumbh = Kumbh_Sans({
+    weight: [ "400", "500", "600", "700", "800" ],
+    display: 'swap',
+    subsets: ['latin']
+})
 
 const layout = ({ children }) => {
     return (
-        <div>{children}</div>
+        <div className={ kumbh.className }>
+            <Header />
+            <main>
+                { children }
+            </main>
+            <Footer />
+        </div>
     )
 }
 
---
+++ components/Application/Website/Footer.jsx
@@ -0,0 +1,154 @@
+import Image from 'next/image'
+import React from 'react'
+import logo from '@/public/assets/images/logo-black.png'
+import Link from 'next/link'
+import { WEBSITE_HOME, WEBSITE_LOGIN, WEBSITE_REGISTER } from '@/routes/WebsiteRoute'
+import { IoLocationOutline } from "react-icons/io5";
+import { MdOutlinePhone } from "react-icons/md";
+import { MdOutlineEmail } from "react-icons/md";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { TiSocialFacebookCircular } from "react-icons/ti";
+import { FiTwitter } from "react-icons/fi";
+
+
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-50 border-t'>
+      <div
+        className="grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 gap-10 py-10 lg:px-32 px-4">
+
+        <div className="lg:col-span-1 md:col-span-2 col-span-1">
+          <Image
+            src={logo}
+            weight={383}
+            height={146}
+            alt='logo'
+            className='w-36 mb-2' />
+          <p className="text-gray-500 text-sm">
+            E-store is your trusted destination for quality and convenience. From fashion to essentials, we bring everything you need right to your doorstep. Stop, smart, ive better - only at E-store.
+          </p>
+        </div>
+
+        <div>
+          <h4 className="text-xl font-bold uppercase mb-5">Categories</h4>
+          <ul>
+            <li className="mb-2 text-gray-500">
+              <Link href="">T-shirt</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Hoodies</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Oversized</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Full Sleeves</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Polo</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4 className="text-xl font-bold uppercase mb-5">Useful Links</h4>
+          <ul>
+            <li className="mb-2 text-gray-500">
+              <Link href={WEBSITE_HOME}>Home</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Shop</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">About</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href={WEBSITE_REGISTER}>Register</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href={WEBSITE_LOGIN}>Login</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4 className="text-xl font-bold uppercase mb-5">Help Center</h4>
+          <ul>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Register</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Login</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">My Account</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Privacy Policy</Link>
+            </li>
+            <li className="mb-2 text-gray-500">
+              <Link href="">Terms & Conditions</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4 className="text-xl font-bold uppercase mb-5">Contact Us</h4>
+          <ul>
+            <li className="mb-2 text-gray-500 flex gap-2">
+              <IoLocationOutline size={20} />
+              <span className='text-sm'>
+                E-store market Kapilvastu, Nepal 32800
+              </span>
+            </li>
+            <li className="mb-2 text-gray-500 flex gap-2">
+              <MdOutlinePhone size={20} />
+              <Link
+                href="+977-9876543212"
+                className='hover:text-primary text-sm' >
+                +977-9876543212
+              </Link>
+            </li>
+            <li className="mb-2 text-gray-500 flex gap-2">
+              <MdOutlineEmail size={20} />
+              <Link
+                href="mailto:support@estore.com"
+                className='hover:text-primary text-sm' >
+                support@estore.com
+              </Link>
+            </li>
+          </ul>
+
+          <div className='flex gap-5 mt-5'>
+            <Link href="">
+              <AiOutlineYoutube className="text-primary" size={25} />
+            </Link>
+            <Link href="">
+              <FaInstagram className="text-primary" size={25} />
+            </Link>
+            <Link href="">
+              <FaWhatsapp className="text-primary" size={25} />
+            </Link>
+            <Link href="">
+              <TiSocialFacebookCircular className="text-primary" size={25} />
+            </Link>
+            <Link href="">
+              <FiTwitter className="text-primary" size={25} />
+            </Link>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="py-5 bg-gray-100">
+        <p className='text-center'> © 2025 Estore. All Rights Reversed.</p>
+      </div>
+    </footer>
+  )
+}
+
+
+export default Footer

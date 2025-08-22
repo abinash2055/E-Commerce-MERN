66d60bdb53c44a16aa0acbdc19fb34768c004400
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2025-08-22 22:02:08 +0545

    Admin dashboard Static to Dynamic

--- app/(root)/(admin)/admin/dashboard/OrderOverview.jsx
+++ app/(root)/(admin)/admin/dashboard/OrderOverview.jsx
@@ -17,22 +17,38 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart"
+import { useEffect, useState } from "react"
+import useFetch from "@/hooks/useFetch"
 
 export const description = "A bar chart"
 
-const chartData = [
-  { month: "January", amount: 186 },
-  { month: "February", amount: 305 },
-  { month: "March", amount: 237 },
-  { month: "April", amount: 73 },
-  { month: "May", amount: 209 },
-  { month: "June", amount: 214 },
-  { month: "July", amount: 554 },
-  { month: "August", amount: 158 },
-  { month: "September", amount: 327 },
-  { month: "October", amount: 214 },
-  { month: "November", amount: 200 },
-  { month: "December", amount: 597 },
+// const chartData = [
+//   { month: "January", amount: 186 },
+//   { month: "February", amount: 305 },
+//   { month: "March", amount: 237 },
+//   { month: "April", amount: 73 },
+//   { month: "May", amount: 209 },
+//   { month: "June", amount: 214 },
+//   { month: "July", amount: 554 },
+//   { month: "August", amount: 158 },
+//   { month: "September", amount: 327 },
+//   { month: "October", amount: 214 },
+//   { month: "November", amount: 200 },
+//   { month: "December", amount: 597 },
+// ]
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "November",
+  "December",
 ]
 
 const chartConfig = {
@@ -40,28 +56,47 @@ const chartConfig = {
     label: "Amount",
     color: "#8e51ff",
   },
-} 
+}
 
 export function OrderOverview() {
+
+  const [chartData, setChartData] = useState([])
+  const { data: response } = useFetch('/api/dashboard/admin/monthly-sales')
+
+  useEffect(() => {
+    if (monthlySales && monthlySales.success) {
+      const getChartData = months.map((month, index) => {
+        const monthData = monthlySales.data.find(item => item._id.month === index + 1)
+
+        return {
+          month: month,
+          amount: monthData ? monthData.totalSales : 0
+        }
+      })
+
+      setChartData(getChartData)
+    }
+  }, [monthlySales])
+
   return (
     <div>
-    <ChartContainer config={chartConfig}>
-      <BarChart accessibilityLayer data={chartData}>
-        <CartesianGrid vertical={false} />
-        <XAxis
-          dataKey="month"
-          tickLine={false}
-          tickMargin={10}
-          axisLine={false}
-          tickFormatter={(value) => value.slice(0, 3)}
-        />
-        <ChartTooltip
-          cursor={true}
-          content={<ChartTooltipContent />}
-        />
-        <Bar dataKey="amount" fill="var(--color-amount)" radius={5} />
-      </BarChart>
-    </ChartContainer>
+      <ChartContainer config={chartConfig}>
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <ChartTooltip
+            cursor={true}
+            content={<ChartTooltipContent />}
+          />
+          <Bar dataKey="amount" fill="var(--color-amount)" radius={5} />
+        </BarChart>
+      </ChartContainer>
     </div>
   )
 }
--- app/(root)/(admin)/admin/dashboard/OrderStatus.jsx
+++ app/(root)/(admin)/admin/dashboard/OrderStatus.jsx
@@ -7,17 +7,19 @@ import {
     ChartTooltip,
     ChartTooltipContent,
 } from "@/components/ui/chart"
+import { useEffect, useState } from "react"
+import useFetch from "@/hooks/useFetch"
 
 export const description = "A donut chart"
 
-const chartData = [
-    { status: "pending", count: 275, fill: "var(--color-pending)" },
-    { status: "processing", count: 200, fill: "var(--color-processing)" },
-    { status: "shipped", count: 187, fill: "var(--color-shipped)" },
-    { status: "delivered", count: 173, fill: "var(--color-delivered)" },
-    { status: "cancelled", count: 90, fill: "var(--color-cancelled)" },
-    { status: "unverified", count: 90, fill: "var(--color-unverified)" },
-]
+// const chartData = [
+//     { status: "pending", count: 275, fill: "var(--color-pending)" },
+//     { status: "processing", count: 200, fill: "var(--color-processing)" },
+//     { status: "shipped", count: 187, fill: "var(--color-shipped)" },
+//     { status: "delivered", count: 173, fill: "var(--color-delivered)" },
+//     { status: "cancelled", count: 90, fill: "var(--color-cancelled)" },
+//     { status: "unverified", count: 90, fill: "var(--color-unverified)" },
+// ]
 
 const chartConfig = {
     status: {
@@ -50,6 +52,35 @@ const chartConfig = {
 }
 
 export function OrderStatus() {
+
+    const [chartData, setChartData] = useState([])
+    const [statusCount, setStatusCount] = useState([])
+
+    const [totalCount, setTotalCount] = useState(0)
+    const { data: orderStatus, loading } = useFetch('/api/dashboard/admin/order-status')
+
+    useEffect(() => {
+        if (orderStatus && orderStatus.success) {
+            const newOrderStatus = orderStatus.data.map((o) => ({
+                status: o._id,
+                count: o.count,
+                fill: `var(--colot-${o._id})`
+            }))
+
+            setChartData(newOrderStatus)
+
+            const getTotalCount = orderStatus.data.reduce((acc, curr) => acc + curr.count, 0)
+            setTotalCount(getTotalCount)
+
+            const statusObj = orderStatus.data.reduce((acc, item) => {
+                acc[item._id] = item.count
+                return acc
+            }, {})
+
+            setStatusCount(statusObj)
+        }
+    }, [orderStatus])
+
     return (
         <div>
             <ChartContainer
@@ -82,7 +113,7 @@ export function OrderStatus() {
                                                 y={viewBox.cy}
                                                 className="fill-foreground text-3xl font-bold"
                                             >
-                                                100
+                                                {totalCount}
                                             </tspan>
                                             <tspan
                                                 x={viewBox.cx}
@@ -98,7 +129,7 @@ export function OrderStatus() {
                         />
                     </Pie>
 
-                    
+
                 </PieChart>
             </ChartContainer>
 
@@ -106,27 +137,27 @@ export function OrderStatus() {
                 <ul>
                     <li className="flex justify-between items-center mb-3 text-sm">
                         <span>Pending</span>
-                        <span className="rounded-full px-2 text-sm bg-blue-500 text-white">0</span>
+                        <span className="rounded-full px-2 text-sm bg-blue-500 text-white">{statusCount?.pending || 0}</span>
                     </li>
                     <li className="flex justify-between items-center mb-3 text-sm">
                         <span>Processing</span>
-                        <span className="rounded-full px-2 text-sm bg-yellow-500 text-white">0</span>
+                        <span className="rounded-full px-2 text-sm bg-yellow-500 text-white">{statusCount?.processing || 0}</span>
                     </li>
                     <li className="flex justify-between items-center mb-3 text-sm">
                         <span>Shipped</span>
-                        <span className="rounded-full px-2 text-sm bg-cyan-500 text-white">0</span>
+                        <span className="rounded-full px-2 text-sm bg-cyan-500 text-white">{statusCount?.shipped || 0}</span>
                     </li>
                     <li className="flex justify-between items-center mb-3 text-sm">
                         <span>Delivered</span>
-                        <span className="rounded-full px-2 text-sm bg-green-500 text-white">0</span>
+                        <span className="rounded-full px-2 text-sm bg-green-500 text-white">{statusCount?.delivered || 0}</span>
                     </li>
                     <li className="flex justify-between items-center mb-3 text-sm">
                         <span>Cancelled</span>
-                        <span className="rounded-full px-2 text-sm bg-red-500 text-white">0</span>
+                        <span className="rounded-full px-2 text-sm bg-red-500 text-white">{statusCount?.cancelled || 0}</span>
                     </li>
                     <li className="flex justify-between items-center mb-3 text-sm">
                         <span>Unverified</span>
-                        <span className="rounded-full px-2 text-sm bg-orange-500 text-white">0</span>
+                        <span className="rounded-full px-2 text-sm bg-orange-500 text-white">{statusCount?.unverified || 0}</span>
                     </li>
                 </ul>
             </div>
